814c021e703aa563ad10b08672b9c33b1114f6b6
Everton Almeida <everton.o.almeida@outlook.com>
2016-12-02 15:11:27 -0200

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
- const {ObjectID} = require('mongodb');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -19,7 +20,7 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
@@ -30,27 +31,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET /todos/1231321
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid(id)){
-     return res.status(404).send();
-   }
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
 
-   Todo.findById(id).then((todo) => {
-     if(!todo){
-       return res.status(404).send();
-     }
-     res.send({todo});
-   }).catch((e) => {
-     re.status(400).send();
-   });
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
-
 
 module.exports = {app};
